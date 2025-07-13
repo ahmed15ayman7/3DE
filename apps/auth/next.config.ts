affb7c6480e180444f5731d42ef21e9a39b7573f
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-07-13 20:04:40 +0300

    update packege.json

--- apps/auth/next.config.ts
+++ apps/auth/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/auth',
+  trailingSlash: true,
+  output: 'standalone',
   transpilePackages: ['@3de/ui', '@3de/apis', '@3de/interfaces', '@3de/auth'],
   experimental: {
     optimizePackageImports: ['@3de/ui', 'lucide-react'],
