367c91d2d6187af0d75955cc82c7843a226246e0
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-07-13 21:48:32 +0300

    update style

--- apps/auth/next.config.ts
+++ apps/auth/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/auth',
+  assetPrefix: '/auth/',
   trailingSlash: true,
   output: 'standalone',
   transpilePackages: ['@3de/ui', '@3de/apis', '@3de/interfaces', '@3de/auth'],
