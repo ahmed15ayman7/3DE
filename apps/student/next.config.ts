8279b764489468602921ff02fda218c8201bb2d9
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-07-13 20:52:08 +0300

    update style

--- apps/student/next.config.ts
+++ apps/student/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/student',
+  assetPrefix: '/student/',
   trailingSlash: true,
   output: 'standalone',
   transpilePackages: ['@3de/ui', '@3de/apis', '@3de/interfaces', '@3de/auth'],
