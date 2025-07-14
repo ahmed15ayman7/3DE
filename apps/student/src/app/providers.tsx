6ecea51be6114e7078ef3c50e79de13ef21b1d25
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-07-14 14:52:41 +0300

    add loading

--- apps/student/src/app/providers.tsx
+++ apps/student/src/app/providers.tsx
@@ -19,9 +19,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <Suspense fallback={<RouteLoader showText loadingText="طريق النجاح يبدأ بخطوة... ويستمر بالصبر" size="md" />}>
+        {/* <Suspense fallback={<RouteLoader showText loadingText="طريق النجاح يبدأ بخطوة... ويستمر بالصبر" size="md" />}> */}
         {children}
-        </Suspense>
+        {/* </Suspense> */}
           
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </AuthProvider>
