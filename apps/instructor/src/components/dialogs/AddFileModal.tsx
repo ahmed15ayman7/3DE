049e78a7f7dfd4f16f73b29a85f52d6d258115ca
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-08-12 22:54:15 +0300

    update backend

--- apps/instructor/src/components/dialogs/AddFileModal.tsx
+++ apps/instructor/src/components/dialogs/AddFileModal.tsx
@@ -82,7 +82,7 @@ const AddFileModal = ({
         setUploadProgress(0);
   
         const uploadToast = toast(
-          <div className='flex flex-col gap-2 w-full' style={{width: '400px'}}>
+          <div className='flex flex-col gap-2 w-full' style={{width: '300px'}}>
           <Progress
           value={uploadProgress}
           showLabel={true}
