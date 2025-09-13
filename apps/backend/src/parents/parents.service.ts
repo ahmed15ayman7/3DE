8f3fc79e587e9d5ab6a748f035e797e58c6cc85f
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-09-13 05:05:06 +0300

    update parents

--- apps/backend/src/parents/parents.service.ts
+++ apps/backend/src/parents/parents.service.ts
@@ -35,6 +35,7 @@ export class ParentsService {
                 },
             },
             include: {
+                user: true,
                 children: {
                     include: {
                         user: true,
